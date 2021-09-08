3eba4fe7d758af61860c3b3b171b5fbefaedf13f
MaximilianGeorgiev <scissorhands.tw@gmail.com>
2021-09-08 14:42:12 +0300

    Add block-related API calls

--- server.js
+++ server.js
@@ -1,10 +1,45 @@
+const {SSL_OP_EPHEMERAL_RSA} = require('constants');
 const express = require('express');
+const {reset} = require('nodemon');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const Web3 = require('web3');
 
-app.get('/',function(req,res) {
-    res.end('Hello World!')
+const web3 = new Web3('https://polygon-rpc.com/');
+
+// block hash or block num as parameter (both work)
+app.get('/getblock/:arg', (req, res) => {
+    web3.eth.getBlock(req.params.arg, true, (error, result) => { // TO DO: handle error
+    }).then(value => {
+        res.json(value);
+    })
 });
 
-app.listen(PORT, ()=>{console.log(`Server listening on http://localhost:${PORT}`)});
+// fetch Y blocks after X blocks are skipped (most recent block first)
+app.get('/getblocks/:from/:count', (req, res) => {
+    web3.eth.getBlock('latest', false, (error, result) => {}).then((value) => {
+        const startBlockNumber = value.number - req.params.from; // get latest block number for calculations
+        const blockCount = req.params.count;
+
+        let fetchedBlocks = [];
+
+        for (let i = startBlockNumber; i >= startBlockNumber - blockCount; i--) {
+            web3.eth.getBlock(i, false, (error, result) => {}).then((value) => {
+                fetchedBlocks.push(value);
+
+                if (i == startBlockNumber - blockCount) {
+                    res.end(JSON.stringify(fetchedBlocks));
+                }
+            })
+        }
+    });
+});
+
+app.get('/', function (req, res) {
+    res.end('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+});
